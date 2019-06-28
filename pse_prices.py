2d7da84138da5c60b996cb2136b70d30736d2583
wctarrayo <w.tarrayo@gmail.com>
2019-06-28 13:57:14 +0800

    finished pse_prices.py code

--- pse_prices.py
+++ pse_prices.py
@@ -1,19 +1,48 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 from cnx import DB_PSE
 import datetime
 import time
 
-# expects tuple with 3 elements, a) symbol, b) company_id, c) security_id
-def GetHistorical(company):
+def GetHistorical(company, browser):
+    """
+    Expects tuple with 3 elements, a) symbol, b) company_id, c) security_id
+    Returns a list containing 30 days worth of stock prices for the given company
+    """
+
+    # explode company details to 3 variables
     symbol, company_id, security_id = company
+
+    # prepare web url
     url = "https://pse.com.ph/stockMarket/companyInfo.html?id={}&security={}&tab=3".format(company_id, security_id)
-    print(symbol)
-    print(url)
+    
+    print('Fetching prices for {}...'.format(symbol))
+
+    # load web page for company
+    browser.get(url)
+    time.sleep(10)
+
+
+    # prepare an empty list that will hold the list of prices and will be used to return the list
+    prices = []
+
+    # get the prices for the past 30 days for the given company
+    for rows in browser.find_elements_by_class_name('x-grid3-row-table'):
+        details = rows.text.splitlines()
+        details.insert(0, symbol)
+
+        # convert date to ISO format
+        details[1] = datetime.datetime.strptime(details[1], '%b %d, %Y').strftime('%Y-%m-%d')
+
+        # remove commas from numeric data
+        for i in range(7):
+            details[i + 2] = details[i + 2].replace(',','')
+
+        prices.append(details)
+        
+    # return the list of prices
+    return prices
 
 
 # connect to database
@@ -32,10 +61,43 @@ cursor.close()
 cnx.close()
 
 # limit results for testing
-companies = companies [:1]
+# companies = companies [:1]
+
+# set browser options
+driver_path = '/media/Code/Scrapers/bin/geckodriver'
+options = Options()
+options.set_headless(True)
+
+# load the website
+browser = webdriver.Firefox(options=options, executable_path=driver_path)
 
 
 # iterate through all the results
+prices = None
 for company in companies:
-    GetHistorical(company)
+    prices = GetHistorical(company, browser)
+
+    # prepare database connection
+    cnx = DB_PSE()
+    cursor = cnx.cursor()
+
+    # upload prices
+    for price in prices:
+
+        # prepare sql command
+        sql = "INSERT INTO prices (symbol, trade_date, price_open, price_high, price_low, price_close, price_ave, volume, trade_value) VALUES {} ON DUPLICATE KEY UPDATE price_ave={}".format(tuple(price), price[6])
+        print('Inserting {} prices for {} trading day.'.format(price[0], price[1]))
+        # print(sql)
+        cursor.execute(sql)
+        cnx.commit()
+
+    cursor.close()
+    cnx.close()
+
+# release the browser
+browser.quit()
+
+    
+
+
 
--- pse_symbols.py
+++ pse_symbols.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from cnx import DB_PSE
 import datetime
 import time
