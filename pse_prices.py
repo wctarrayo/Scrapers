5013d62e70a6dd09a19365123b518cddee999561
wctarrayo <w.tarrayo@gmail.com>
2019-06-29 14:45:41 +0800

    added logic to skip symbols that has been processed for the day

--- pse_prices.py
+++ pse_prices.py
@@ -20,8 +20,9 @@ def GetHistorical(company, browser):
     print('Fetching prices for {}...'.format(symbol))
 
     # load web page for company
+    browser.implicitly_wait(5)
     browser.get(url)
-    time.sleep(10)
+    # time.sleep(10)
 
 
     # prepare an empty list that will hold the list of prices and will be used to return the list
@@ -44,13 +45,19 @@ def GetHistorical(company, browser):
     # return the list of prices
     return prices
 
+# get date today
+dt_today = datetime.date.today().strftime('%Y-%m-%d')
 
 # connect to database
 cnx = DB_PSE()
 cursor = cnx.cursor()
 
-# prepare SQL command
-sql = "SELECT symbol, company_id, security_id FROM companies ORDER BY symbol"
+# prepare SQL command, get symbols that haven' been processed for the day
+sql = ( "SELECT c.symbol, c.company_id, c.security_id "
+        "FROM companies c "
+        "LEFT JOIN done_list d ON d.symbol=c.symbol AND d.today=\'{}\' "
+        "WHERE ISNULL(d.symbol) "
+        "ORDER BY c.symbol").format(dt_today)
 
 # get SQL results and store in a variable
 cursor.execute(sql)
@@ -81,16 +88,28 @@ for company in companies:
     cnx = DB_PSE()
     cursor = cnx.cursor()
 
+    # initialize symbol to None indicating that it hasn't been processed yet
+    symbol = None
+
     # upload prices
     for price in prices:
 
+        symbol = price[0]
+
         # prepare sql command
         sql = "INSERT INTO prices (symbol, trade_date, price_open, price_high, price_low, price_close, price_ave, volume, trade_value) VALUES {} ON DUPLICATE KEY UPDATE price_ave={}".format(tuple(price), price[6])
-        print('Inserting {} prices for {} trading day.'.format(price[0], price[1]))
+        print('Inserting {} prices for {} trading day.'.format(symbol, price[1]))
         # print(sql)
         cursor.execute(sql)
         cnx.commit()
 
+    # if symbol has been processed put entry in the database
+    if symbol != None:
+        sql = "INSERT INTO done_list (symbol, today) VALUES (\'{}\', \'{}\') ON DUPLICATE KEY UPDATE today=\'{}\'".format(symbol, dt_today, dt_today)
+        cursor.execute(sql)
+        cnx.commit()
+
+    # close database connections
     cursor.close()
     cnx.close()
 
